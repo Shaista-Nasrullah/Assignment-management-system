9eac6597808f3d4934d4c77283901818ad3779ee
Shaista-Nasrullah <106731221+Shaista-Nasrullah@users.noreply.github.com>
2024-12-09 20:44:06 +0500

    initial

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //configure env
 dotenv.config();
@@ -22,18 +26,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, '../client/build')));
-// app.use(express.static(path.join(__dirname, './client/build')))
-
-
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use("*", function (req, res){
+app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+});
+
+
 
 //PORT
 const PORT = process.env.PORT || 8000;
